63c3f3a872f81d7261a5bf045b6a3adb694a5573
Santtu Lintervo <santervo@gmail.com>
2011-04-30 21:21:55 +0300

    vimclojure

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject querybuilder "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]])
